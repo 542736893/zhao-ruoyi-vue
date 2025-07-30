0bee96a2c7bfa39c4af7b49b83bba47216d67cab
zhaozhibin <5297395+zhaozhibin123@user.noreply.gitee.com>
2025-07-30 22:08:41 +0800

    init push

---
+++ ruoyi-server/src/main/java/org/example/ruoyiserver/RuoyiServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.ruoyiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiServerApplication.class, args);
+    }
+
+}
---
+++ ruoyi-server/src/test/java/org/example/ruoyiserver/RuoyiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ruoyiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
